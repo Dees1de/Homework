28f53f0dffbea956173828abe6cb8d5893debee5
Dees1de <alexandr.bocharov2@gmail.com>
2024-06-25 18:03:30 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+example='Университет'
+
+print(example[0])
+
+print(example[-1])
+
+a=int(len(example)/2)
+print(example[a:])
+
+print(example[::-1])
+
+print(example[1::2])
---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_homeworks = 12
+used_time = 1.5
+course = 'Python'
+time_for_1_task = used_time/completed_homeworks
+
+print("Курс: ",course,', всего задач:',completed_homeworks,', затрачено часов: ',used_time,', среднее время выполнения ', time_for_1_task,' часа')

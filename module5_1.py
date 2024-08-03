e7f63c858de20212021bbb6635dab36d6bede1e9
Dees1de <alexandr.bocharov2@gmail.com>
2024-08-03 13:12:50 +0500

    Add files via upload

---
+++ module5_1.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+        self.house_info()
+
+    def house_info(self):
+        print(f'В жилищном комплексе {self.name}, {self.number_of_floor} этажей ')
+
+
+    def go_to(self, new_floor):
+        for i in range(new_floor):
+            if new_floor > self.number_of_floor:
+                print(f'{new_floor} этажа в "{self.name}" не существует')
+                break
+            else:
+                print(i+1)
+
+
+h1 = House('ЖК Горский', 18)
+h1.go_to(5)
+h2 = House('Домик в деревне', 2)
+h2.go_to(10)
